004ca34264453d759aa2c069a77e895e7621be62
Jimmy Nguyen <nguyen89.mke@gmail.com>
2024-10-06 01:48:37 -0500

    Page Addition: Tournaments

--- app/tournaments/[name].js/page.js
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-    return (
-        <div>Specific Tournament</div>
-    );
-}
--- app/tournaments/page.js
+++ app/tournaments/page.js
@@ -1,7 +1,42 @@
+import styles from './tournaments.module.css';
+
 export default function Page() {
+    const data = {
+        tournaments: [
+            {
+                name: "Rock O' Ween",
+                date: "October 31, 2021",
+                location: "Mosh Performance Center",
+            },
+            {
+                name: 'Christmas Classic',
+                date: "December 25, 2021",
+                location: "Mosh Performance Center",
+            },
+            {
+                name: 'Spring Fling',
+                date: "March 20, 2022",
+                location: "Mosh Performance Center",
+            }
+        ]
+    }
+
+    const allTourneys = data.tournaments.map(tourney => {
+        return (
+            <div key={tourney.name} className={styles.tourneyCard}>
+                <div className={styles.cardHeader}>
+                    <h3>{tourney.name}</h3>
+                    <p>{tourney.date}</p>
+                </div>
+                <p>{tourney.location}</p>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            <h1>All Tournament Page</h1>
+        <div className={styles.container}>
+            <h1>All Tournaments</h1>
+            {allTourneys}
         </div>
     )
 }
